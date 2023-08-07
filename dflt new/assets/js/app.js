9b7965cd5d95d338b5e751972a21ed9dc37f81e4
ywang289 <56518323+ywang289@users.noreply.github.com>
2023-08-07 11:25:13 -0400

    can show the input on the screen

--- dflt new/assets/js/app.js
+++ dflt new/assets/js/app.js
@@ -16,29 +16,20 @@ const validType = {
 }
 
 // user inputs elements
-let firstnameElem = mainForm.board_type,
-    middlenameElem = mainForm.ball_size,
-    lastnameElem = mainForm.displayTable,
-    imageElem = mainForm.paste_type,
-    designationElem = mainForm.paste_size,
-    addressElem = mainForm.displayTable2,
-    emailElem = mainForm.edu_start_date,
-    phonenoElem = mainForm.reflow_temp,
-    summaryElem = mainForm.reflow_time;
+let choice = document.getElementById('typeSection').value;
+let boardtypeElem = mainForm.board_type,
+    ballsizeElem = mainForm.ball_size,
+    pastetypeElem= mainForm.paste_type
+    pastesizeElem= mainForm.paste_size,
+    reflowtempElem= mainForm.reflow_temp,
+    reflowtimeElem= mainForm.reflow_time;
+
+
 
 // display elements
-let nameDsp = document.getElementById('fullname_dsp'),
-    imageDsp = document.getElementById('image_dsp'),
-    phonenoDsp = document.getElementById('phoneno_dsp'),
-    emailDsp = document.getElementById('email_dsp'),
-    addressDsp = document.getElementById('address_dsp'),
-    designationDsp = document.getElementById('designation_dsp'),
-    summaryDsp = document.getElementById('summary_dsp'),
-    projectsDsp = document.getElementById('projects_dsp'),
-    achievementsDsp = document.getElementById('achievements_dsp'),
-    skillsDsp = document.getElementById('skills_dsp'),
-    educationsDsp = document.getElementById('educations_dsp'),
-    experiencesDsp = document.getElementById('experiences_dsp');
+
+let nameDsp = document.getElementById('fullname_dsp');
+    
 
 // first value is for the attributes and second one passes the nodelists
 const fetchValues = (attrs, ...nodeLists) => {
@@ -61,76 +52,26 @@ const fetchValues = (attrs, ...nodeLists) => {
 }
 
 const getUserInputs = () => {
+    
+       
+        boardtypeElem.addEventListener('keyup', (e) => validateFormData(e.target, validType.TEXT, 'board_type'));
+        ballsizeElem.addEventListener('keyup', (e) => validateFormData(e.target, validType. DIGIT, 'ball_size'));
+        pastetypeElem.addEventListener('keyup', (e) => validateFormData(e.target, validType.TEXT, 'paste_type'));
+        pastesizeElem.addEventListener('keyup', (e) => validateFormData(e.target, validType. DIGIT, 'paste_size'));
+        reflowtempElem.addEventListener('keyup', (e) => validateFormData(e.target, validType. DIGIT, 'reflow_temp'));
+        reflowtimeElem.addEventListener('keyup', (e) => validateFormData(e.target, validType. DIGIT, 'reflow_time'));
+    
 
-    // achivements 
-    let achievementsTitleElem = document.querySelectorAll('.achieve_title'),
-    achievementsDescriptionElem = document.querySelectorAll('.achieve_description');
+        return {
+           board_type: boardtypeElem.value
+        , ballsize: ballsizeElem.value
+        ,pastetype:pastetypeElem.value,
+        pastesize:pastesizeElem.value,
+        reflow_temp:reflowtempElem.value,
+        reflow_time:reflowtimeElem.value
 
-    // experiences
-    let expTitleElem = document.querySelectorAll('.exp_title'),
-    expOrganizationElem = document.querySelectorAll('.exp_organization'),
-    expLocationElem = document.querySelectorAll('.exp_location'),
-    expStartDateElem = document.querySelectorAll('.exp_start_date'),
-    expEndDateElem = document.querySelectorAll('.exp_end_date'),
-    expDescriptionElem = document.querySelectorAll('.exp_description');
+        }
 
-    // education
-    let eduSchoolElem = document.querySelectorAll('.edu_school'),
-    eduDegreeElem = document.querySelectorAll('.edu_degree'),
-    eduCityElem = document.querySelectorAll('.edu_city'),
-    eduStartDateElem = document.querySelectorAll('.edu_start_date'),
-    eduGraduationDateElem = document.querySelectorAll('.edu_graduation_date'),
-    eduDescriptionElem = document.querySelectorAll('.edu_description');
-
-    let projTitleElem = document.querySelectorAll('.proj_title'),
-    projLinkElem = document.querySelectorAll('.proj_link'),
-    projDescriptionElem = document.querySelectorAll('.proj_description');
-
-    let skillElem = document.querySelectorAll('.skill');
-
-    // event listeners for form validation
-    firstnameElem.addEventListener('keyup', (e) => validateFormData(e.target, validType.TEXT, 'board_type'));
-    middlenameElem.addEventListener('keyup', (e) => validateFormData(e.target, validType.TEXT_EMP, 'Middle Name'));
-    lastnameElem.addEventListener('keyup', (e) => validateFormData(e.target, validType.TEXT, 'Last Name'));
-    phonenoElem.addEventListener('keyup', (e) => validateFormData(e.target, validType.PHONENO, 'Phone Number'));
-    emailElem.addEventListener('keyup', (e) => validateFormData(e.target, validType.EMAIL, 'Email'));
-    addressElem.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Address'));
-    designationElem.addEventListener('keyup', (e) => validateFormData(e.target, validType.TEXT, 'Designation'));
-
-    achievementsTitleElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Title')));
-    achievementsDescriptionElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Description')));
-    expTitleElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Title')));
-    expOrganizationElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Organization')));
-    expLocationElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, "Location")));
-    expStartDateElem.forEach(item => item.addEventListener('blur', (e) => validateFormData(e.target, validType.ANY, 'End Date')));
-    expEndDateElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'End Date')));
-    expDescriptionElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Description')));
-    eduSchoolElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'School')));
-    eduDegreeElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Degree')));
-    eduCityElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'City')));
-    eduStartDateElem.forEach(item => item.addEventListener('blur', (e) => validateFormData(e.target, validType.ANY, 'Start Date')));
-    eduGraduationDateElem.forEach(item => item.addEventListener('blur', (e) => validateFormData(e.target, validType.ANY, 'Graduation Date')));
-    eduDescriptionElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Description')));
-    projTitleElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Title')));
-    projLinkElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Link')));
-    projDescriptionElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Description')));
-    skillElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'skill')));
-
-    return {
-        firstname: firstnameElem.value,
-        middlename: middlenameElem.value,
-        lastname: lastnameElem.value,
-        designation: designationElem.value,
-        address: addressElem.value,
-        email: emailElem.value,
-        phoneno: phonenoElem.value,
-        summary: summaryElem.value,
-        achievements: fetchValues(['achieve_title', 'achieve_description'], achievementsTitleElem, achievementsDescriptionElem),
-        experiences: fetchValues(['exp_title', 'exp_organization', 'exp_location', 'exp_start_date', 'exp_end_date', 'exp_description'], expTitleElem, expOrganizationElem, expLocationElem, expStartDateElem, expEndDateElem, expDescriptionElem),
-        educations: fetchValues(['edu_school', 'edu_degree', 'edu_city', 'edu_start_date', 'edu_graduation_date', 'edu_description'], eduSchoolElem, eduDegreeElem, eduCityElem, eduStartDateElem, eduGraduationDateElem, eduDescriptionElem),
-        projects: fetchValues(['proj_title', 'proj_link', 'proj_description'], projTitleElem, projLinkElem, projDescriptionElem),
-        skills: fetchValues(['skill'], skillElem)
-    }
 };
 
 function validateFormData(elem, elemType, elemName){
@@ -249,17 +190,16 @@ const showListData = (listData, listContainer) => {
 }
 
 const displayCV = (userData) => {
-    nameDsp.innerHTML = userData.firstname + " " + userData.middlename + " " + userData.lastname;
-    phonenoDsp.innerHTML = userData.phoneno;
-    emailDsp.innerHTML = userData.email;
-    addressDsp.innerHTML = userData.address;
-    designationDsp.innerHTML = userData.designation;
-    summaryDsp.innerHTML = userData.summary;
-    showListData(userData.projects, projectsDsp);
-    showListData(userData.achievements, achievementsDsp);
-    showListData(userData.skills, skillsDsp);
-    showListData(userData.educations, educationsDsp);
-    showListData(userData.experiences, experiencesDsp);
+   
+    ballDsp.innerHTML ="the board type is: "+ userData.board_type  + " the ballsize is " + userData.ballsize;
+    pasteDsp.innerHTML ="the paste type is: "+ userData.paste_type  + " the pastesize is " + userData.paste_size;
+    reflowDsp.innerHTML=" the reflow temp is " + userData.reflow_temp + " the reflow timw is "+ userData.reflow_time;
+   
+    
+    // showListData(userData.achievements, achievementsDsp);
+    // showListData(userData.skills, skillsDsp);
+    // showListData(userData.educations, educationsDsp);
+    // showListData(userData.experiences, experiencesDsp);
 }
 
 // generate CV
